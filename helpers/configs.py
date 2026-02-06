cce78cd45d9e4caaf67940fd82c973ff7843a10a
Ross Derewianko <rderewianko@gmail.com>
2026-02-06 13:03:01 -0800

    Update tenant URL domain from kandji.io to iru.com for Slack links

--- helpers/configs.py
+++ helpers/configs.py
@@ -276,7 +276,7 @@ class Configurator:
             sys.exit(1)
 
         # Assign tenant URL
-        self.tenant_url = self.kandji_api_url.replace(".api.", ".")
+        self.tenant_url = self.kandji_api_url.replace(".api.", ".").replace("kandji.io", "iru.com")
         # Assign API domain
         self.kandji_api_prefix = os.path.join(self.kandji_api_url, "api", "v1")
         # Define API endpoints
